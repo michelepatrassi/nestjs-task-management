2b2819e74d2fa0bb8a9380d75c02da730e7febd2
Michele Patrassi <michele.patrassi@gmail.com>
2020-06-08 21:08:22 +0200

    basic logging

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,43 +1,73 @@
 import { Task } from './task.entity';
-import { Repository, EntityRepository } from "typeorm";
+import { Repository, EntityRepository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { User } from '../auth/user.entity';
+import { Logger, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-    async getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Task[]> {
-        const {status, search} = filterDto;
-        const qb = this.createQueryBuilder('task');
+  private logger = new Logger('TaskRepository');
 
-        qb.where('task.userId = :userId', {userId: user.id})
+  async getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Task[]> {
+    const { status, search } = filterDto;
+    const qb = this.createQueryBuilder('task');
 
-        if (status) {
-            qb.andWhere('task.status = :status', {status})
-        }
+    qb.where('task.userId = :userId', { userId: user.id });
 
-        if (search) {
-            qb.andWhere('(task.title LIKE :search OR task.description LIKE :search)', {search: `%${search}%`})
-        }
-
-        const tasks = await qb.getMany();
-
-        return tasks;
+    if (status) {
+      qb.andWhere('task.status = :status', { status });
     }
 
-    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
-        const {title, description} = createTaskDto;
-
-        const task = new Task();
-        task.title = title;
-        task.description = description;
-        task.status = TaskStatus.OPEN;
-        task.user = user;
-        await task.save();
-
-        delete task.user;
-
-        return task;
+    if (search) {
+      qb.andWhere(
+        '(task.title LIKE :search OR task.description LIKE :search)',
+        { search: `%${search}%` },
+      );
     }
+
+    try {
+      const tasks = await qb.getMany();
+
+      return tasks;
+    } catch (e) {
+      this.logger.error(
+        `Failed to get tasks for user ${user.id}. Filters ${JSON.stringify(
+          filterDto,
+          null,
+          2,
+        )}`,
+        e.stack,
+      );
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = new Task();
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+    task.user = user;
+    try {
+      await task.save();
+
+      delete task.user;
+
+      return task;
+    } catch (e) {
+      this.logger.error(
+        `Failed to create task for user ${user.id}. Filters ${JSON.stringify(
+          createTaskDto,
+          null,
+          2,
+        )}`,
+        e.stack,
+      );
+      throw new InternalServerErrorException();
+    }
+  }
 }
