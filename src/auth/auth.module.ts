7253700fe1d935d3208f855132152a7f51a0d882
Michele Patrassi <michele.patrassi@gmail.com>
2020-06-08 21:31:36 +0200

    config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,18 +3,24 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
-import {PassportModule} from '@nestjs/passport';
-import {JwtModule} from '@nestjs/jwt';
-import { SECRET } from './constants';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
-    PassportModule.register({defaultStrategy: 'jwt'}),
-    JwtModule.register({secret: SECRET, signOptions: {expiresIn: 3600}}),
-    TypeOrmModule.forFeature([UserRepository])],
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
+      signOptions: { expiresIn: jwtConfig.expiresIn },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
-  exports: [JwtStrategy, PassportModule]
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,30 +1,30 @@
 import { JwtPayload } from './jwt-payload.interface';
-import { PassportStrategy } from "@nestjs/passport";
-import {Strategy, ExtractJwt} from 'passport-jwt';
-import { SECRET } from './constants';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { UnauthorizedException, Injectable } from '@nestjs/common';
 import { User } from './user.entity';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(
-        @InjectRepository(UserRepository) private userRepository: UserRepository
-    ) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: SECRET
-        })
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user = await this.userRepository.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException();
     }
 
-    async validate(payload: JwtPayload): Promise<User> {
-        const {username} = payload;
-        const user = await this.userRepository.findOne({username});
-        if (!user) {
-            throw new UnauthorizedException();
-        }
-
-        return user;
-    }
+    return user;
+  }
 }
