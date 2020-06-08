f013be203276b754b8a1a433f7eebec53bf5ad2f
Michele Patrassi <michele.patrassi@gmail.com>
2020-06-08 22:24:02 +0200

    updated package to prevent prod issue

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,37 +1,48 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany } from "typeorm";
-import * as bcrypt from 'bcrypt';
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+  OneToMany,
+} from 'typeorm';
+import * as bcrypt from 'bcryptjs';
 import { Task } from '../tasks/task.entity';
 
 @Entity()
 @Unique(['username'])
 export class User extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    username: string;
+  @Column()
+  username: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    @Column()
-    salt: string;
+  @Column()
+  salt: string;
 
-    @OneToMany(type => Task, task => task.user, {eager: true})
-    tasks: Task[]
+  @OneToMany(
+    type => Task,
+    task => task.user,
+    { eager: true },
+  )
+  tasks: Task[];
 
-    async validatePassword(password: string): Promise<boolean> {
-        const pw = await bcrypt.hash(password, this.salt);
-        return pw === this.password;
-    }
+  async validatePassword(password: string): Promise<boolean> {
+    const pw = await bcrypt.hash(password, this.salt);
+    return pw === this.password;
+  }
 
-    static generateMock(): User {
-        const user = new User();
-        user.id = 111111;
-        user.username = 'username';
-        user.password = 'password';
-        user.salt = 'salt';
-        user.tasks = [];
-        return user;
-    }
+  static generateMock(): User {
+    const user = new User();
+    user.id = 111111;
+    user.username = 'username';
+    user.password = 'password';
+    user.salt = 'salt';
+    user.tasks = [];
+    return user;
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,45 +1,45 @@
-import { Repository, EntityRepository } from "typeorm";
+import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { ConflictException, InternalServerErrorException } from "@nestjs/common";
-import * as bcrypt from 'bcrypt';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        const {username, password} = authCredentialsDto;
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
 
-        const user = new User();
-        user.username = username;
-        user.salt = await bcrypt.genSalt();
-        user.password = await this.hashPassword(password, user.salt);
+    const user = new User();
+    user.username = username;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
-        try {
-            await user.save();
-        }
-        catch(e) {
-            if (e.code === '23505') {
-                throw new ConflictException('username already exists');
-            }
-            throw new InternalServerErrorException();
-        }
-        
+    try {
+      await user.save();
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException('username already exists');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+    console.log(`user.repository.ts`, user);
+    if (user && (await user.validatePassword(password))) {
+      const { password, ...userData } = user;
+      return userData;
     }
 
-    async validateUserPassword(authCredentialsDto: AuthCredentialsDto) {
-        const {username, password} = authCredentialsDto;
-        const user = await this.findOne({username});
-        console.log(`user.repository.ts`, user);
-        if (user && await user.validatePassword(password)) {
-            const {password, ...userData} = user;
-            return userData;
-        }
+    return null;
+  }
 
-        return null;
-    }
-
-    private async hashPassword(password: string, salt: string): Promise<string> {
-        return bcrypt.hash(password, salt);
-    }
-
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
 }
